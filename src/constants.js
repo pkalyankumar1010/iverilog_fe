688cd03e71a13ca875d989391f5d6dfb6091f24c
pkalyankumar1010 <pkalyankumar1010@gmail.com>
2025-01-26 16:56:36 +0530

    langauge

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,16 @@
 export const LANGUAGE_VERSIONS = {
+  verilog: "1.0.0",
   javascript: "18.15.0",
   typescript: "5.0.3",
   python: "3.10.0",
   java: "15.0.2",
   csharp: "6.12.0",
   php: "8.2.3",
-  verilog: "1.0.0",
 };
 
 export const CODE_SNIPPETS = {
+  verilog:
+    'module test; \ninitial begin \n$display("Hello, Verilog");\nend endmodule',
   javascript: `\nfunction greet(name) {\n\tconsole.log("Hello, " + name + "!");\n}\n\ngreet("Alex");\n`,
   typescript: `\ntype Params = {\n\tname: string;\n}\n\nfunction greet(data: Params) {\n\tconsole.log("Hello, " + data.name + "!");\n}\n\ngreet({ name: "Alex" });\n`,
   python: `\ndef greet(name):\n\tprint("Hello, " + name + "!")\n\ngreet("Alex")\n`,
@@ -16,6 +18,4 @@ export const CODE_SNIPPETS = {
   csharp:
     'using System;\n\nnamespace HelloWorld\n{\n\tclass Hello { \n\t\tstatic void Main(string[] args) {\n\t\t\tConsole.WriteLine("Hello World in C#");\n\t\t}\n\t}\n}\n',
   php: "<?php\n\n$name = 'Alex';\necho $name;\n",
-  verilog:
-    'module test; \ninitial begin \n$display("Hello, Verilog");\nend endmodule',
 };
